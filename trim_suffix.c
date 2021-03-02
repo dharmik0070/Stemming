4987ef0903303217f49d9242c39a33ac14cc2228
agarwalayushi <42030009+agarwalayushi@users.noreply.github.com>
2021-03-02 22:34:47 +0530

    Update trim_suffix.c

--- trim_suffix.c
+++ trim_suffix.c
@@ -10,11 +10,15 @@
 int i;
 char* s = NULL;
 char* str;
-
+/*
+Function name: trim_suffix
+Purpose: This function takes the input as a string and return the root word.
+*/
 void  trim_suffix(char* s)
 {
 	FILE* fp1;
 	char root[50];
+	//Allocating the memory to 'str' variable.
 	str = (char*)malloc(sizeof(char*) * strlen(s));
 	str = return_str(s);
 	if (str == NULL) {
@@ -25,6 +29,7 @@ void  trim_suffix(char* s)
 		while (fgets(root, sizeof(root), fp1))
 		{
 			root[strcspn(root, "\n")] = 0;
+			//Comparing the string variable root & str.
 			if (strcmp(root, str) == 0)
 			{
 				printf("\n  Word After Removing Suffix is :  %s", str);
@@ -33,7 +38,10 @@ void  trim_suffix(char* s)
 	}
 
 }
-
+/*
+Function name: return_str
+Purpose: Checking different condition by calling functions included in different steps.
+*/
 char* return_str(char* s)
 {
 	char* temp;
@@ -48,8 +56,8 @@ char* return_str(char* s)
 	return s;
 }
 /*
-function name: cons
-Purpose: check characters in string is vowel or consonant
+Function name: cons
+Purpose: Check characters in string is vowel or consonant.
 */
 int cons(int i, char* str)
 {
@@ -62,9 +70,9 @@ int cons(int i, char* str)
 }
 
 /*
-function name: degree_m
-Purpose: to count the value of m
-	      m=vc (occurance of vowel and consonant)
+Function name: degree_m
+Purpose: 1. To count the value of m
+	 2. m=vc (occurance of vowel and consonant)
 */
 int degree_m(char* str)
 {
@@ -73,8 +81,10 @@ int degree_m(char* str)
 	length = strlen(str);
 	for (i = 0; i < length - 1; i++)
 	{
+		//Checking Vowel character.
 		if (!cons(i, str))
 		{
+			//Checking Consonant right after the vowel.
 			if (cons(i + 1, str) && length > 0)
 			{
 				count++;
@@ -85,30 +95,33 @@ int degree_m(char* str)
 }
 
 /*
-function name: vowel_stem
-Purpose: check if stem contains vowel or not
+Function name: vowel_stem
+Purpose: Check if stem contains vowel or not
 */
 int vowel_stem(char* str)
 {
 	int flag = 0;
 	for (i = 0; str[i]; i++)
 	{
+		//Checking vowel character.
 		if (!cons(i, str))
 		{
 			flag = 1;
 		}
 	}
+	//Return '1' if string contains a vowel, '0' otherwise.
 	return flag;
 }
 
 /*
-function name: cons
-Purpose: check if stem ends with double consonant or not
+Function name: cons
+Purpose: Check if stem ends with double consonant or not.
 */
 
 int double_cons(char* str) {
 	int length;
 	length = strlen(str);
+	//Checking last two characters of string as consonants with same value.
 	if (cons(length - 1, str) && str[length - 1] == str[length - 2])
 	{
 		return 1;
@@ -117,16 +130,18 @@ int double_cons(char* str) {
 }
 
 /*
-function name: cons
-Purpose: check if stem ends with cvc(occurance of consonant vowel consonant)
+Function name: cons_vowel_cons
+Purpose: Check if stem ends with cvc(occurance of consonant vowel consonant).
 */
 int cons_vowel_cons(char* str) {
 	int length;
 	char ch;
 	length = strlen(str);
 	ch = str[length - 1];
+	//Checking the last three characters of string as consonant_vowel_consonant.
 	if (cons(length - 1, str) && !cons(length - 2, str) && cons(length - 3, str))
 	{
+		//Checking the last character of consonant should not be 'w' , 'x' or 'y'.
 		if (ch == 'w' || ch == 'x' || ch == 'y')
 			return 0;
 		return 1;
@@ -135,8 +150,8 @@ int cons_vowel_cons(char* str) {
 }
 
 /*
-function name: end_s
-Purpose: check stem ends with character or not
+Function name: end_s
+Purpose: Check stem ends with character or not
 */
 int end_s(char* str, char ch)
 {
@@ -150,22 +165,24 @@ int end_s(char* str, char ch)
 }
 
 /*
-function name: cleanup_1
-Purpose: 1. if word ends with 'at' then replace with 'ate'
-		 2. if word ends with 'bl' then replace with 'ble'
-		 3. if word ends with double consonant and not ends with 'l','s','z' then replace double consonant with single letter
+Function name: cleanup_1
+Purpose: 1. If word ends with 'at' then replace with 'ate'.
+	 2. If word ends with 'bl' then replace with 'ble'.
+	 3. If word ends with double consonant and not ends with 'l','s','z' then replace double consonant with single letter.
 */
 
 char* cleanup_1(char* sr)
 {
 	int length;
 	length = strlen(sr);
+	//Checking the last two character of string are 'a' and 't'.
 	if (sr[length - 1] == 't' && sr[length - 2] == 'a')
 	{
 		sr[length] = 'e';
 		sr[length + 1] = '\0';
 
 	}
+	//Checking the last two character of string are 'b' and 'l'.
 	else if (sr[length - 1] == 'l' && sr[length - 2] == 'b')
 	{
 		sr[length] = 'e';
@@ -173,11 +190,13 @@ char* cleanup_1(char* sr)
 
 	}
 
+	//Checking the last character of string should not end with 'l','s' or 'z' and satisfy the double consonant condition.
 	else if (!(end_s(sr, 'l') || end_s(sr, 's') || end_s(sr, 'z')) && double_cons(sr))
 	{
 		sr[length - 1] = '\0';
 
 	}
+	//Checking the degree of m should equal to '1' and satisfy the consonant_vowel_consonant condition.
 	else if (degree_m(sr) == 1 && cons_vowel_cons(sr))
 	{
 
@@ -188,32 +207,36 @@ char* cleanup_1(char* sr)
 }
 
 /*
-function name: step1_a_b
-Purpose: 1. if word ends with 'sses' then replace it with 'ss'
-		 2. if word ends with 'ies' then replace with 'i'
-		 3. if word ends with 's' then replace with 'NULL'
-		 4. if value of m>1 and word ends with 'eed' then replace with 'ee'
-		 5. if stem contains vowel and stem ends with 'ed' then replace with 'NULL'
-		 6. if stem contains vowel and stem ends with 'ing' then replace with 'NULL'
-		 7. call cleanup_1()
+Function name: step1_a_b
+Purpose: 1. If word ends with 'sses' then replace it with 'ss'
+	 2. If word ends with 'ies' then replace with 'i'
+	 3. If word ends with 's' then replace with 'NULL'
+	 4. If value of m>1 and word ends with 'eed' then replace with 'ee'
+	 5. If stem contains vowel and stem ends with 'ed' then replace with 'NULL'
+	 6. If stem contains vowel and stem ends with 'ing' then replace with 'NULL'
+	 7. Call cleanup_1()
 */
 char* step1_a_b(char* temp)
 {
 	s = NULL;
 	int length;
 	length = strlen(temp);
+	//Checking the last character of string is 's'.
 	if (end_s(temp, 's'))
 	{
+		//Checking the last two characters of string are 'i' and 'e'.
 		if (temp[length - 2] == 'e' && temp[length - 3] == 'i')
 		{
 			temp[length - 2] = '\0';
 			s = temp;
 		}
+		//Checking the last three characters of string are 's','s' and 'e'.
 		else if (temp[length - 2] == 'e' && temp[length - 3] == 's' && temp[length - 4] == 's')
 		{
 			temp[length - 2] = '\0';
 			s = temp;
 		}
+		//Checking the last character of string is 's'.
 		else if (temp[length - 2] == 's')
 		{
 			s = temp;
@@ -223,6 +246,7 @@ char* step1_a_b(char* temp)
 			s = temp;
 		}
 	}
+	//Checking the last three characters of string are 'e','e' and 'd'.
 	else if (temp[length - 1] == 'd' && temp[length - 2] == 'e' && temp[length - 3] == 'e')
 	{
 		char eed[30];
@@ -235,6 +259,7 @@ char* step1_a_b(char* temp)
 			s = temp;
 		}
 	}
+	//Checking the last two characters of string are 'e' and 'd'.
 	else if (temp[length - 1] == 'd' && temp[length - 2] == 'e')
 	{
 
@@ -249,6 +274,7 @@ char* step1_a_b(char* temp)
 			}
 		}
 	}
+	//Checking the last three characters of string are 'i','n' and 'g'.
 	else if (temp[length - 1] == 'g' && temp[length - 2] == 'n' && temp[length - 3] == 'i')
 	{
 		char ing[30];
@@ -267,14 +293,15 @@ char* step1_a_b(char* temp)
 
 }
 /*
-function name: step_1_c
-Purpose: 1. if stem contains vowel and stem ends with 'y' then replace it with 'i'		 
+Function name: step_1_c
+Purpose: 1. If stem contains vowel and stem ends with 'y' then replace it with 'i'		 
 */
 char* step_1_c(char* temp)
 {
 	s = NULL;
 	int length;
 	length = strlen(temp);
+	//Checking the presence of vowel and string ends with 'y'.
 	if (vowel_stem(temp) && temp[length - 1] == 'y')
 	{
 		temp[length - 1] = 'i';
@@ -286,10 +313,10 @@ char* step_1_c(char* temp)
 }
 
 /*
-function name: step_2
-Purpose: 1. if value of m>0 and word ends with 'ational' then replace with 'ate'
-		 2.  if value of m>0 and word ends with 'ization' then replace with 'ize'
-		 3.  if value of m>0 and word ends with 'bilite' then replace with 'ble'
+Function name: step_2
+Purpose: 1. If value of m>0 and word ends with 'ational' then replace with 'ate'
+	 2.  If value of m>0 and word ends with 'ization' then replace with 'ize'
+	 3.  If value of m>0 and word ends with 'bilite' then replace with 'ble'
 		 
 */
 
@@ -301,6 +328,7 @@ char* step_2(char* temp) {
 	strcpy(step2, temp);
 	if (strlen(step2) > 7) {
 		strcpy(step2, substring_return(step2, 7));
+		
 		if ((length > 7 && degree_m(step2) > 0) &&
 			(temp[length - 1] == 'l' && temp[length - 2] == 'a' &&
 				temp[length - 3] == 'n' && temp[length - 4] == 'o' &&
@@ -310,6 +338,7 @@ char* step_2(char* temp) {
 			temp[length - 4] = '\0';
 			s = temp;
 		}
+		//Checking the degree of m>0 and string ends with 'ational'.
 		if (temp[length - 1] == 'l' && temp[length - 2] == 'a' &&
 			temp[length - 3] == 'n' && temp[length - 4] == 'o' &&
 			temp[length - 5] == 'i' && temp[length - 6] == 't')
@@ -321,6 +350,7 @@ char* step_2(char* temp) {
 				s = temp;
 			}
 		}
+		//Checking the degree of m>0 and string ends with 'ization'.
 		else if ((length > 7 && degree_m(step2) > 0) &&
 			(temp[length - 1] == 'n' && temp[length - 2] == 'o' &&
 				temp[length - 3] == 'i' && temp[length - 4] == 't' &&
@@ -331,6 +361,7 @@ char* step_2(char* temp) {
 			s = temp;
 		}
 
+		//Checking the string ends with 'biliti'.
 		else if ((length > 6) && temp[length - 1] == 'i' &&
 			temp[length - 2] == 't' && temp[length - 3] == 'i' &&
 			temp[length - 4] == 'l' && temp[length - 5] == 'i' && temp[length - 6] == 'b')
@@ -350,10 +381,10 @@ char* step_2(char* temp) {
 }
 
 /*
-function name: step_3
-Purpose: 1. if value of m>0 and word ends with 'icate' then replace with 'ic'
-		 2. if value of m>0 and word ends with 'ful' then replace with 'NULL'
-		 3. if value of m>0 and word ends with 'ness' then replace with 'NULL'
+Function name: step_3
+Purpose: 1. If value of m>0 and word ends with 'icate' then replace with 'ic'
+	 2. If value of m>0 and word ends with 'ful' then replace with 'NULL'
+         3. If value of m>0 and word ends with 'ness' then replace with 'NULL'
 
 */
 
@@ -365,6 +396,7 @@ char* step_3(char* temp) {
 	if (strlen(step3) > 5) {
 
 		strcpy(step3, substring_return(step3, 5));
+		//Checking the degree of m>0 and string ends with 'icate'.
 		if ((length > 3 && degree_m(step3) > 0) &&
 			(temp[length - 1] == 'e' && temp[length - 2] == 't' &&
 				temp[length - 3] == 'a' && temp[length - 4] == 'c' && temp[length - 5] == 'i'))
@@ -372,6 +404,7 @@ char* step_3(char* temp) {
 			temp[length - 3] = '\0';
 			s = temp;
 		}
+		//Checking the string ends with 'ful'.
 		else if ((temp[length - 1] == 'l' && temp[length - 2] == 'u' && temp[length - 3] == 'f'))
 		{
 			strcpy(step3, temp);
@@ -382,6 +415,7 @@ char* step_3(char* temp) {
 			}
 
 		}
+		//Checking the string ends with 'ness'.
 		else if (temp[length - 1] == 's' && temp[length - 2] == 's' &&
 			temp[length - 3] == 'e' && temp[length - 4] == 'n')
 		{
@@ -397,10 +431,10 @@ char* step_3(char* temp) {
 	printf("\nIn step 3, final value of s is %s\n", s);
 }
 /*
-function name: step_4
-Purpose: 1. if value of m>0 and word ends with 'ance' then replace with 'NULL'
-		 2. if value of m>0 and word ends with 'ent' then replace with 'NULL'
-		 3. if value of m>0 and word ends with 'ive' then replace with 'NULL'
+Function name: step_4
+Purpose: 1. If value of m>0 and word ends with 'ance' then replace with 'NULL'
+	 2. If value of m>0 and word ends with 'ent' then replace with 'NULL'
+	 3. If value of m>0 and word ends with 'ive' then replace with 'NULL'
 
 */
 
@@ -413,6 +447,7 @@ char* step_4(char* temp) {
 	if (strlen(step4) > 4) {
 
 		strcpy(step4, substring_return(step4, 4));
+		//Checking the length of string and m>0 and string ends with 'ance'.
 		if ((length > 4 && degree_m(step4) > 0) &&
 			(temp[length - 1] == 'e' && temp[length - 2] == 'c' &&
 				temp[length - 3] == 'n' && temp[length - 4] == 'a'))
@@ -420,6 +455,7 @@ char* step_4(char* temp) {
 			temp[length - 4] = '\0';
 			s = temp;
 		}
+		//Checking the length of string and string ends with 'ent'.
 		else if (length > 3 && temp[length - 1] == 't' && temp[length - 2] == 'n' && temp[length - 3] == 'e')
 		{
 			strcpy(step4, temp);
@@ -429,6 +465,7 @@ char* step_4(char* temp) {
 				s = temp;
 			}
 		}
+		//Checking the length of string and string ends with 'ive'.
 		else if (length > 3 && temp[length - 1] == 'e' && temp[length - 2] == 'v' && temp[length - 3] == 'i')
 		{
 			strcpy(step4, temp);
@@ -444,10 +481,10 @@ char* step_4(char* temp) {
 }
 
 /*
-function name: step_5
-Purpose: 1. if value of m>1 and word ends with 'e' then replace with 'NULL'
-		 2. if value of m=0 and in end word not contain cvc form and ends with 'ness' then replace with 'NULL'
-		 3. if m>1 and word ends with double consonant and word ends with 'l' then it replace with single letter
+Function name: step_5
+Purpose: 1. If value of m>1 and word ends with 'e' then replace with 'NULL'.
+	 2. If value of m=0 and in end word not contain cvc form and ends with 'ness' then replace with 'NULL'.
+	 3. If m>1 and word ends with double consonant and word ends with 'l' then it replace with single letter.
 */
 
 char* step_5(char* temp)
@@ -458,11 +495,13 @@ char* step_5(char* temp)
 	strcpy(step5, temp);
 	if (strlen(step5) > 4) {
 		strcpy(step5, substring_return(step5, 1));
+		//Checking the degree m>1 and string ends with 'e'.
 		if (length > 1 && degree_m(step5) > 1 && (temp[length - 1] == 'e'))
 		{
 			temp[length - 1] = '\0';
 			s = temp;
 		}
+		//Checking string end with 'ness'.
 		else if (length > 4 && end_s(temp, 's') && (temp[length - 2] == 's') &&
 			(temp[length - 3] == 'e') && (temp[length - 4] == 'n'))
 		{
@@ -474,6 +513,7 @@ char* step_5(char* temp)
 			}
 		}
 
+		//Checking degree of m>1 and string ends with double consonant and ends with 'l'.
 		else if (length > 2 && double_cons(temp) && end_s(temp, 'l'))
 		{
 			strcpy(step5, temp);
@@ -488,7 +528,7 @@ char* step_5(char* temp)
 
 }
 /*
-function name: substring_return
+Function name: substring_return
 Purpose: 
 
 */
